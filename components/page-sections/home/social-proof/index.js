8965cd7b1cab959912bf6a8331eea30098eff589
jai-sharma <sendtojsharma@gmail.com>
2021-03-10 19:08:45 +0530

    adding social-proof section

---
+++ components/page-sections/home/social-proof/index.js
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss';
+
+export default function SocialProof() {
+    return (
+        <section className={styles.section}>
+            
+        </section>
+    )
+}
